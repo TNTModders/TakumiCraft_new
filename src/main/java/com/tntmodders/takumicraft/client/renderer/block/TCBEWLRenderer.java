9f633380310b54738f374bce88e3edbb7b467886
tomkate <tomkate@tntmodders.com>
2024-05-05 11:08:14 +0900

    匠式防盾の描画修正

--- src/main/java/com/tntmodders/takumicraft/client/renderer/block/TCBEWLRenderer.java
+++ src/main/java/com/tntmodders/takumicraft/client/renderer/block/TCBEWLRenderer.java
@@ -18,7 +18,6 @@ import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.blockentity.BannerRenderer;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.ModelBakery;
@@ -46,20 +45,7 @@ public class TCBEWLRenderer extends BlockEntityWithoutLevelRenderer {
         this.shieldModel = new TCShieldModel(Minecraft.getInstance().getEntityModels().bakeLayer(TCRenderCore.SHIELD));
     }
 
- /*   static void renderPatterns(PoseStack poseStack, MultiBufferSource bufferSource, int p_112077_, int p_112078_, ModelPart modelPart, Material material, boolean p_112081_, List<Pair<Holder<BannerPattern>, DyeColor>> p_112082_, boolean p_112083_) {
-        modelPart.render(poseStack, bufferSource.getBuffer(RenderType.entitySolid(new ResourceLocation(TakumiCraftCore.MODID, "textures/entity/shield.png"))), p_112077_, p_112078_);
-
-        for (int i = 0; i < 17 && i < p_112082_.size(); ++i) {
-            final int factor = i;
-            Pair<Holder<BannerPattern>, DyeColor> pair = p_112082_.get(i);
-            float[] afloat = pair.getSecond().getTextureDiffuseColors();
-            pair.getFirst().unwrapKey().map(p_234428_ -> p_112081_ ? Sheets.getBannerMaterial(p_234428_) : Sheets.getShieldMaterial(p_234428_)).ifPresent(p_234425_ -> modelPart.render(poseStack, p_234425_.buffer(bufferSource, RenderType::entityNoOutline), p_112077_, p_112078_, afloat[0], afloat[1], afloat[2], 0.75f));
-        }
-
-    }*/
-
-    public static void renderPatterns(PoseStack p_112075_, MultiBufferSource p_112076_, int p_112077_, int p_112078_, ModelPart p_112079_, Material p_112080_, boolean p_112081_, DyeColor p_336347_, BannerPatternLayers p_332113_, boolean p_112083_
-    ) {
+    public static void renderPatterns(PoseStack p_112075_, MultiBufferSource p_112076_, int p_112077_, int p_112078_, ModelPart p_112079_, Material p_112080_, boolean p_112081_, DyeColor p_336347_, BannerPatternLayers p_332113_, boolean p_112083_) {
         p_112079_.render(p_112075_, p_112076_.getBuffer(RenderType.entitySolid(new ResourceLocation(TakumiCraftCore.MODID, "textures/entity/shield.png"))), p_112077_, p_112078_);
         renderPatternLayer(p_112075_, p_112076_, p_112077_, p_112078_, p_112079_, p_112081_ ? Sheets.BANNER_BASE : Sheets.SHIELD_BASE, p_336347_);
 
@@ -70,11 +56,9 @@ public class TCBEWLRenderer extends BlockEntityWithoutLevelRenderer {
         }
     }
 
-    private static void renderPatternLayer(
-            PoseStack p_332210_, MultiBufferSource p_336119_, int p_333952_, int p_335632_, ModelPart p_327937_, Material p_327979_, DyeColor p_331652_
-    ) {
+    private static void renderPatternLayer(PoseStack p_332210_, MultiBufferSource p_336119_, int p_333952_, int p_335632_, ModelPart p_327937_, Material p_327979_, DyeColor p_331652_) {
         float[] afloat = p_331652_.getTextureDiffuseColors();
-        p_327937_.render(p_332210_, p_327979_.buffer(p_336119_, RenderType::entityNoOutline), p_333952_, p_335632_, afloat[0], afloat[1], afloat[2], 1.0F);
+        p_327937_.render(p_332210_, p_327979_.buffer(p_336119_, RenderType::entityNoOutline), p_333952_, p_335632_, afloat[0], afloat[1], afloat[2], 0.75F);
     }
 
 
@@ -103,11 +87,10 @@ public class TCBEWLRenderer extends BlockEntityWithoutLevelRenderer {
                 VertexConsumer vertexconsumer = bufferSource.getBuffer(RenderType.entitySolid(new ResourceLocation(TakumiCraftCore.MODID, "textures/entity/shield.png")));
                 this.shieldModel.handle().render(poseStack, vertexconsumer, p_108834_, p_108835_, 1.0F, 1.0F, 1.0F, 1.0F);
                 if (flag) {
-                    BannerRenderer.renderPatterns(poseStack, bufferSource, p_108834_, p_108835_, this.shieldModel.plate(), material, false, Objects.requireNonNullElse(dyecolor, DyeColor.WHITE), bannerpatternlayers, stack.hasFoil());
+                    renderPatterns(poseStack, bufferSource, p_108834_, p_108835_, this.shieldModel.plate(), material, false, Objects.requireNonNullElse(dyecolor, DyeColor.WHITE), bannerpatternlayers, stack.hasFoil());
                 } else {
                     this.shieldModel.plate().render(poseStack, vertexconsumer, p_108834_, p_108835_, 1.0F, 1.0F, 1.0F, 1.0F);
                 }
-
                 poseStack.popPose();
             }
             case BlockItem blockItem when blockItem.getBlock() instanceof TCCreeperBedBlock bedBlock -> {
