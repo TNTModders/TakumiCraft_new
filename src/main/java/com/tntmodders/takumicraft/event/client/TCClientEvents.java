2abbf8b460510a634b80b0bedaa9d4360bf1d050
tomkate <tomkate@tntmodders.com>
2024-05-29 21:38:27 +0900

    DebugModeのときExperimentalメニューを無視するオプションを追加

--- src/main/java/com/tntmodders/takumicraft/event/client/TCClientEvents.java
+++ src/main/java/com/tntmodders/takumicraft/event/client/TCClientEvents.java
@@ -2,11 +2,16 @@ package com.tntmodders.takumicraft.event.client;
 
 import com.tntmodders.takumicraft.TakumiCraftCore;
 import com.tntmodders.takumicraft.client.screen.TCTakumiBookScreen;
+import com.tntmodders.takumicraft.core.TCConfigCore;
 import com.tntmodders.takumicraft.core.client.TCKeyBindingCore;
+import com.tntmodders.takumicraft.utils.TCLoggingUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.BackupConfirmScreen;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -21,6 +26,21 @@ public class TCClientEvents {
             if (event.getKey() == TCKeyBindingCore.MAP_TAKUMIBOOK.getKey().getValue()) {
                 Minecraft.getInstance().setScreen(new TCTakumiBookScreen());
             }
+        }
+    }
+
+    @SubscribeEvent
+    public void ScreenRender(ScreenEvent.Init event) {
+        if (TCConfigCore.TCCommonConfig.COMMON.isDebugMode.get() && event.getScreen() instanceof BackupConfirmScreen screen) {
+            screen.renderables.forEach(renderable -> {
+                if (renderable instanceof Button button) {
+                    String string = button.getMessage().toString();
+                    if (string.contains("selectWorld.backupJoinSkipButton")) {
+                        TCLoggingUtils.info("skipped");
+                        button.onPress();
+                    }
+                }
+            });
         }
     }
 }
